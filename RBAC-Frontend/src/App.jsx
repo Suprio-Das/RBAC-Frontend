6b6be2ae540a25ef576abd7bf094a354bdfbad66
Suprio-Das <priyamd507@gmail.com>
2025-07-25 00:11:22 +0600

    Login route created

--- RBAC-Frontend/src/App.jsx
+++ RBAC-Frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router'
 import './App.css'
 import Signup from './Components/Signup'
+import Login from './Components/Login'
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path='/register' element={<Signup></Signup>}></Route>
+          <Route path='/login' element={<Login></Login>}></Route>
         </Routes>
       </BrowserRouter>
     </>
