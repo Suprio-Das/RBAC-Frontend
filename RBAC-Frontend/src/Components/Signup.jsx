c8f491fb7afe19ec1797afa901f3deb7be1f9566
Suprio-Das <priyamd507@gmail.com>
2025-07-24 22:45:21 +0600

    Basic signup form created

--- RBAC-Frontend/src/Components/Signup.jsx
+++ RBAC-Frontend/src/Components/Signup.jsx
@@ -1,7 +1,24 @@
 const Signup = () => {
     return (
         <div className="min-h-screen flex justify-center items-center">
-            <h1>Signup page.</h1>
+            <div>
+                <h1 className="text-3xl text-center font-bold">Signup</h1>
+                <form action="">
+                    <fieldset className="fieldset">
+                        <legend className="fieldset-legend">Name:</legend>
+                        <input type="text" className="input" placeholder="Type here" name="name" />
+                    </fieldset>
+                    <fieldset className="fieldset">
+                        <legend className="fieldset-legend">Email:</legend>
+                        <input type="email" className="input" placeholder="Type here" name="email" />
+                    </fieldset>
+                    <fieldset className="fieldset">
+                        <legend className="fieldset-legend">Password:</legend>
+                        <input type="password" className="input" placeholder="Type here" name="password" />
+                    </fieldset>
+                    <button className="btn btn-info w-full my-2">Signup</button>
+                </form>
+            </div>
         </div>
     );
 };
