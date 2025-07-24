2f70cd090993fcf9eb9e9e52ecb1f141043f720d
Suprio-Das <priyamd507@gmail.com>
2025-07-25 00:10:16 +0600

    Login page designed

--- RBAC-Frontend/src/Components/Login.jsx
+++ RBAC-Frontend/src/Components/Login.jsx
@@ -1,7 +1,23 @@
 const Login = () => {
+    const handleLogin = (e) => {
+        e.preventDefault();
+    }
     return (
-        <div>
-            <h1>Login Page.</h1>
+        <div className="min-h-screen flex justify-center items-center">
+            <div>
+                <h1 className="text-3xl text-center font-bold">Login</h1>
+                <form onSubmit={handleLogin}>
+                    <fieldset className="fieldset">
+                        <legend className="fieldset-legend">Email:</legend>
+                        <input type="email" className="input" placeholder="Type here" name="email" />
+                    </fieldset>
+                    <fieldset className="fieldset">
+                        <legend className="fieldset-legend">Password:</legend>
+                        <input type="password" className="input" placeholder="Type here" name="password" />
+                    </fieldset>
+                    <button className="btn btn-info w-full my-2">Login</button>
+                </form>
+            </div>
         </div>
     );
 };
