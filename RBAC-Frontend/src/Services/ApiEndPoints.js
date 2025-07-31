229d3c2fcaa75777a7b09c701d0878b91cacd5ef
Suprio-Das <priyamd507@gmail.com>
2025-07-31 22:35:13 +0600

    Interceptor added

--- RBAC-Frontend/src/Services/ApiEndPoints.js
+++ RBAC-Frontend/src/Services/ApiEndPoints.js
@@ -11,3 +11,23 @@ export const get = (url, params) = instance.get(url, { params })
 export const post = (url, data) = instance.post(url, data)
 export const put = (url, data) = instance.put(url, data)
 export const del = (url, data) = instance.delete(url, data)
+
+// Add a request interceptor
+axios.interceptors.request.use(function (config) {
+    // Do something before request is sent
+    return config;
+}, function (error) {
+    // Do something with request error
+    return Promise.reject(error);
+},
+    { synchronous: true, runWhen: () => /* This function returns true */}
+);
+
+// Add a response interceptor
+axios.interceptors.response.use(function onFulfilled(response) {
+    console.log("Interceptor response: ", response);
+    return response;
+}, function onRejected(error) {
+    console.log("Interceptor error: ", error);
+    return Promise.reject(error);
+});
