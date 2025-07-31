f071c5c5969d9b28ba966bb79dc665b69d574599
Suprio-Das <priyamd507@gmail.com>
2025-07-31 22:18:07 +0600

    Custom endpoints created

--- RBAC-Frontend/src/Services/ApiEndPoints.js
+++ RBAC-Frontend/src/Services/ApiEndPoints.js
@@ -6,3 +6,8 @@ const instance = axios.create({
         "Content-Type": "application/json"
     }
 })
+
+export const get = (url, params) = instance.get(url, { params })
+export const post = (url, data) = instance.post(url, data)
+export const put = (url, data) = instance.put(url, data)
+export const del = (url, data) = instance.delete(url, data)
