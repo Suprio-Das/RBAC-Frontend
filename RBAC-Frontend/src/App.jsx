7ca2a4ea8576fddc46fc14fc112666cb630c0a8d
Suprio-Das <priyamd507@gmail.com>
2025-07-31 21:33:16 +0600

    Admin Component and route created

--- RBAC-Frontend/src/App.jsx
+++ RBAC-Frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router'
 import './App.css'
 import Signup from './Components/Signup'
 import Login from './Components/Login'
+import Admin from './Components/Admin'
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path='/register' element={<Signup></Signup>}></Route>
           <Route path='/login' element={<Login></Login>}></Route>
+          <Route path='/admin' element={<Admin></Admin>}></Route>
         </Routes>
       </BrowserRouter>
     </>
