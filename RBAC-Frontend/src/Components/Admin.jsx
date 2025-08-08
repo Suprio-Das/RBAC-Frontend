52c4328db876767dc9f03454f72fc88626cd145f
Suprio-Das <priyamd507@gmail.com>
2025-08-08 21:56:17 +0600

    GetUser function implemented in Admin Dashboard

--- RBAC-Frontend/src/Components/Admin.jsx
+++ RBAC-Frontend/src/Components/Admin.jsx
@@ -1,4 +1,19 @@
+import { useEffect } from "react";
+import { get } from "../Services/ApiEndPoints";
+
 const Admin = () => {
+    useEffect(() => {
+        const GetUser = async () => {
+            try {
+                const request = await get('/api/admin/getuser');
+                const response = request.data;
+                console.log(response);
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        GetUser();
+    }, [])
     return (
         <div>
             <h1>Admin</h1>
