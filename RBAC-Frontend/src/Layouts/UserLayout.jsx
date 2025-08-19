0d2047e149253a2dec4dc0199580a30221bc1a30
Suprio-Das <priyamd507@gmail.com>
2025-08-19 23:11:00 +0600

    Three layouts created

---
+++ RBAC-Frontend/src/Layouts/UserLayout.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const UserLayout = () => {
+    return (
+        <div>
+            <h1>User Layout</h1>
+        </div>
+    );
+};
+
+export default UserLayout;
--- RBAC-Frontend/src/Redux/Store.js
+++ RBAC-Frontend/src/Redux/Store.js
@@ -7,7 +7,7 @@ import { persistReducer, persistStore } from "redux-persist";
 const persistConfig = {
     key: "root",
     storage,
-    whitelist: ['Auth']
+    // whitelist: ['Auth']
 }
 
 // Persist Reducer
